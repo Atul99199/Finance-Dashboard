c7290a8b702e1049d0ab988d959dc5b6a626bc41
Atul Kumar <atulald97@gmail.com>
2026-04-04 20:43:02 +0530

    updated-resposiveness

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,10 +4,10 @@ import { Outlet } from "react-router-dom";
 
 const MainContent = () => {
   return (
-  <div className="p-3 md:p-4 lg:p-5 bg-blue-100 md:rounded-r-xl rounded-t-xl md:rounded-t-none">
+    <div className="p-3 md:p-4 lg:p-5 bg-blue-100 md:rounded-r-xl rounded-t-xl md:rounded-t-none md:h-[95vh] flex flex-col">
       <RightSideNavBar />
-       <hr className="text-white"/>
-      <div className="p-4 ">
+      <hr className="text-white" />
+      <div className="p-4 flex-1 overflow-auto">
         <Outlet />
       </div>
     </div>
